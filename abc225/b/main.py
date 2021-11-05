93ff56d6968f44cd04390012fec25447362a394e
yuuki1036 <yuuki1036@gmail.com>
2021-11-05 11:48:45 +0900

    backup

--- abc225/b/main.py
+++ abc225/b/main.py
@@ -1,2 +1,20 @@
 #!/usr/bin/env python3
 
+n = int(input())
+a1, b1 = input().split()
+arr2 = input().split()
+t = 0
+if a1 in arr2:
+  t = a1
+elif b1 in arr2:
+  t = b1
+
+ans = "Yes"
+
+for i in range(n-3):
+  narr = input().split()
+  if not t in narr:
+    ans = "No"
+    break
+
+print(ans)
