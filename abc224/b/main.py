617ffc1a2b6b1c230ef7f536ba3d03cb93a797bf
yuuki1036 <yuuki1036@gmail.com>
2021-11-10 15:01:25 +0900

    backup

--- abc224/b/main.py
+++ abc224/b/main.py
@@ -1,2 +1,24 @@
 #!/usr/bin/env python3
 
+H, W = [int(i) for i in input().split()]
+M = [[int(i) for i in input().split()] for j in range(H)]
+idxs = []
+
+for i1 in range(H):
+  for i2 in range(H):
+    if i1 >= i2:
+      continue
+    for j1 in range(W):
+      for j2 in range(W):
+        if j1 < j2:
+          idxs.append((i1, i2, j1, j2))
+
+ans = "Yes" if idxs else "No"
+
+for idx in idxs:
+  i1, i2, j1, j2 = idx
+  if M[i1][j1] + M[i2][j2] > M[i2][j1] + M[i1][j2]:
+    ans = "No"
+    break
+
+print(ans)
