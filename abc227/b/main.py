8c832631b013c8609a5faa761423c4f7217812d7
yuuki1036 <yuuki1036@gmail.com>
2021-11-23 23:13:54 +0900

    backup

--- abc227/b/main.py
+++ abc227/b/main.py
@@ -1,2 +1,19 @@
 #!/usr/bin/env python3
 
+N = int(input())
+S = [int(i) for i in input().split()]
+
+ans = 0
+for s in S:
+  ok = False
+  for i in range(1, 201):
+    for j in range(1, 201):
+      if s == (4*i*j) + (3*i) + (3*j):
+        ok = True
+        break
+    if ok:
+      break
+  else:
+    ans += 1
+
+print(ans)
