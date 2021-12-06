d3bc61dc83337a104cdca56151240a1a469900e9
yuuki1036 <yuuki1036@gmail.com>
2021-12-06 11:24:26 +0900

    backup

--- abc228/b/main.py
+++ abc228/b/main.py
@@ -1,2 +1,16 @@
 #!/usr/bin/env python3
 
+n, x = [int(i) for i in input().split()]
+T = [int(i) - 1 for i in input().split()]
+F = [0] * n
+x -= 1
+F[x] = 1
+
+while True:
+  x = T[x]
+  if F[x] == 0:
+    F[x] = 1
+  else:
+    break
+
+print(F.count(1))
