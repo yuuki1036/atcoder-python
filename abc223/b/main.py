9088acaa96617a504a8b64fa52375f042285cad6
yuuki1036 <yuuki1036@gmail.com>
2022-01-11 10:42:28 +0900

    backup

--- abc223/b/main.py
+++ abc223/b/main.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
 
+s = input()
+l = len(s)
+
+if l == 1:
+  print(s)
+  print(s)
+  exit()
+
+S = [s]
+for _ in range(l-1):
+  s = s[1:] + s[0]
+  S.append(s)
+
+S.sort()
+print(S[0])
+print(S[-1])
